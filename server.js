d7e842760fc8d60c6b25e2a71389091c6471c02b
egan <nzukiegan6@gmail.com>
2025-12-03 16:55:58 +0300

    updates

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const paymentsRoutes = require('./routes/payments');
 const penaltiesRoutes = require('./routes/penalties');
 const PORT = process.env.PORT;
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRoutes);
